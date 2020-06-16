612045a1801c3bf5647773bc1dd109723f5afc9c
shanaka <shanakanuwan10@gmail.com>
2020-06-16 21:46:55 +0530

    initial

---
+++ push.js
@@ -0,0 +1,14 @@
+var push = require('web-push');
+
+let vapIdKeys =  { 
+	publicKey: 'BDNL8-x4orwEk6E1W11_Kggw7onYkfnGe-dIlUA49LKa8oYkKEo_GYEBKye6-APUjPu3-yVQ8hcttZ1FDHjMDCE',
+  	privateKey: 'G0qSljtJyhJSYn8y7UMlk0VDpUz3jr-M7JFju5UIO70' }
+
+
+push.setVapidDetails('mailto:lakmal@applimundo.com', vapIdKeys.publicKey, vapIdKeys.privateKey)
+
+let sub = {
+
+}
+
+push.sendNotification(sub, 'test message');
