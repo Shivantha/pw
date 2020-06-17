1385388360c0b3bfea33535fea0c49cb8c442598
shanaka <shanakanuwan10@gmail.com>
2020-06-17 08:09:16 +0530

    change code app.js

---
+++ app.js
@@ -0,0 +1,70 @@
+let isSubscribed = false;
+let swRegistration = null;
+let applicationKey = "BDNL8-x4orwEk6E1W11_Kggw7onYkfnGe-dIlUA49LKa8oYkKEo_GYEBKye6-APUjPu3-yVQ8hcttZ1FDHjMDCE";
+
+
+// Url Encription
+function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
+// Installing service worker
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log('Service Worker and Push is supported');
+
+    navigator.serviceWorker.register('./sw.js')
+        .then(function (swReg) {
+            console.log('service worker registered');
+
+            swRegistration = swReg;
+
+            swRegistration.pushManager.getSubscription()
+                .then(function (subscription) {
+                    isSubscribed = !(subscription === null);
+
+                    console.log(isSubscribed);
+
+                    if (isSubscribed) {
+                        console.log('User is subscribed');
+                    } else {
+                        swRegistration.pushManager.subscribe({
+                                userVisibleOnly: true,
+                                applicationServerKey: urlB64ToUint8Array(applicationKey)
+                            })
+                            .then(function (subscription) {
+                                console.log(subscription);
+                                console.log('User is subscribed');
+                                console.log('hahahahaha')
+
+                                saveSubscription(subscription);
+
+                                isSubscribed = true;
+                            })
+                            .catch(function (err) {
+                                console.log('Failed to subscribe user: ', err);
+                            })
+                    }
+                })
+        })
+        .catch(function (error) {
+            console.error('Service Worker Error', error);
+        });
+} else {
+    console.warn('Push messaging is not supported');
+}
+
+// Send request to database for add new subscriber
+function saveSubscription(subscription) {
+   console.log(subscription);
+}
